7d9ba0ef2c24f4cd6f92bd6b8a4259b1c7e73dba
Clinton Yade <yadeclinton20@gmail.com>
2025-11-14 20:35:15 +0300

    updated SpecialistsPage

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import Hero from "./components/Hero";
-import Services from "./components/Services"
+import Services from "./components/Services";
+import SpecialistsPage from "./components/SpecialistsPage"
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Services />
+      <SpecialistsPage />
 
       
     </main>
